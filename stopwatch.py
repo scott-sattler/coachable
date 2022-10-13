a52b27fa92a776a040f269d12c79000ac5a8b116
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2022-10-12 19:34:18 -0500

    initial commit; provided code

---
+++ stopwatch.py
@@ -0,0 +1,8 @@
+class Stopwatch:
+    # create and start a stop watch
+    def __init__(self):
+        pass
+
+    # returns elapsed time since stopwatch was started
+    def elapsed_time(self):
+        pass
