fe04b96c024abc46070f9e0196bf087af4f3d489
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2022-10-12 20:54:51 -0500

    first solution

--- stopwatch.py
+++ stopwatch.py
@@ -1,8 +1,20 @@
+import time
+
+
 class Stopwatch:
     # create and start a stop watch
-    def __init__(self):
-        pass
+    def __init__(self) -> None:
+        self.__start_time: int = time.perf_counter_ns()
+
+    def restart(self) -> None:
+        self.__start_time = time.perf_counter_ns()
 
     # returns elapsed time since stopwatch was started
-    def elapsed_time(self):
-        pass
+    def elapsed_time(self, resolution: int = 1e-9) -> int:
+        """
+        :param resolution: resolution down to ns; default 1e-9
+        :return: elapsed time since stopwatch was started
+        """
+        elapsed = time.perf_counter_ns() - self.__start_time
+        return elapsed * resolution
+
