acb45cf01317b8bb2a9f2180727b51e235ac341c
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2022-11-11 12:40:50 -0600

    update

---
+++ helper_functions.py
@@ -0,0 +1,37 @@
+
+"""###########################
+######## linked lists ########
+###########################"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    # Provided
+    def __init__(self, val=0, next=None):  # noqa (next shadows built-in name)
+        self.val = val
+        self.next = next
+
+
+def create_linked_list(number: list | tuple | int | str, reverse=False) -> ListNode | None:
+    if isinstance(number, int) or isinstance(number, str):
+        number = list(number)
+    if reverse:
+        number = number[::-1]
+    node = ListNode()
+    head = node
+    for each_element in number:
+        node.next = ListNode()
+        node = node.next
+        node.val = each_element
+    return head.next
+
+
+def print_linked_list(linked_list: ListNode, verbose=True) -> None:
+    node = linked_list
+    print_list = []
+    if verbose:
+        print_list = ['reversed order']
+    while node is not None:
+        print_list.append(node.val)
+        node = node.next
+    print(print_list)
