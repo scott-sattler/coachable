fcaf3796dd22644eb8b6437c53ebf92aa84b8040
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2022-11-23 15:15:00 -0600

    arbitrary save point

--- Coding_Assignments/2_traveling_sales_person/2_traveling_sales_person.py
+++ Coding_Assignments/2_traveling_sales_person/2_traveling_sales_person.py
@@ -1,20 +1,40 @@
+import time
 import os
-from tour import *
-
-import types
-import stopwatch
-import ast
+from point import Point
+from tour import Tour
 
 import plotly.graph_objects as go
+import ast
+
 
 """
-requirements:
-a folder "tsp_test_data" containing the test files
-tour.py must be in the same directory as this file
-tour.py should/must? contain point.py import
+additional requirements:
+- a folder "tsp_test_data" containing the test files
+- tour.py must be in the same directory as this file
+- point.py must be in the same directory as this file
+
+pip install plotly
 """
 # todo: check performance of (reverse + pop last) vs (pop first)
 # todo: implement test_case_validation
+
+
+class Stopwatch:
+    # create and start a stop watch
+    def __init__(self) -> None:
+        self.__start_time: int = time.perf_counter_ns()
+
+    def restart(self) -> None:
+        self.__start_time = time.perf_counter_ns()
+
+    # returns elapsed time since stopwatch was started
+    def elapsed_time(self, resolution: int = 1e-9) -> int:
+        """
+        :param resolution: resolution down to ns; default 1e-9
+        :return: elapsed time (seconds) since stopwatch was started
+        """
+        elapsed = time.perf_counter_ns() - self.__start_time
+        return elapsed * resolution
 
 
 class Test:
@@ -54,7 +74,7 @@ class Test:
         try:
             with open(directory) as file:
                 contents = file.readlines()  # eg ['500 500\n', '200.0 400.0\n', '300.0 100.0\n']
-                points_list: list = []
+                points_list: list = list()
                 for each in contents:
                     points_list.append(tuple(map(float, each.split())))  # split() includes \n
                 test_case = points_list
@@ -64,43 +84,61 @@ class Test:
             return None
 
     @staticmethod
-    def run_test_nearest(test_data: list[tuple[float, float]], reverse_inp: bool = True) -> tuple[float, Tour]:
+    def run_test_nearest(test_data: list[tuple[float, float]],
+                         reverse_inp: bool = True) -> tuple[float, Tour, int]:
+        """
+        :param test_data:
+        :param reverse_inp: reverse input for performance
+        :return: distance, tour, run time
+        """
+        run_time: None | int = None
+        run_timer = Stopwatch()
         if reverse_inp:
             test_data = test_data[::-1]  # reverse list
         tour = Tour()
         while len(test_data) > 0:
             node_data = test_data.pop(-1)
             tour.insert_nearest(Point(node_data[0], node_data[1]))
-        return tour.distance(), tour
+        run_time = run_timer.elapsed_time()
+        return tour.distance(), tour, run_time
 
     @staticmethod
-    def run_test_smallest(test_data: list[tuple[float, float]], reverse_inp: bool = True) -> tuple[float, Tour]:
+    def run_test_smallest(test_data: list[tuple[float, float]],
+                          reverse_inp: bool = True,) -> tuple[float, Tour, int]:
+        """
+        :param test_data:
+        :param reverse_inp: reverse input for performance
+        :return: distance, tour, run time
+        """
+        run_time: None | int = None
+        run_timer = Stopwatch()
         if reverse_inp:
             test_data = test_data[::-1]
         tour = Tour()
+        run_timer.restart()
         while len(test_data) > 0:
             node_data = test_data.pop(-1)
             tour.insert_smallest(Point(node_data[0], node_data[1]))
-        return tour.distance(), tour
+        run_time = run_timer.elapsed_time()
+        return tour.distance(), tour, run_time
 
 
 if __name__ == "__main__":
+    # file_selector indexes test_file_names list
+    file_selector: None | int = 4  # None runs all tests
+
     def truncate(value: float, digits: int) -> float:
         return int(value * (10 ** digits)) / (10 ** digits)
 
     t = Test()
-
-    # file_selector indexes test_file_names list
-    file_selector: None | int = None  # None runs all tests
     tests_to_run = t.test_file_names
     if file_selector is not None:
-        tests_to_run = t.test_file_names[file_selector]
+        tests_to_run = [t.test_file_names[file_selector]]
 
-    for each_file in tests_to_run:
-        file_name = each_file
-        path = t.working_directory + f"/{t.test_folder_name}/" + file_name
+    for file_name in tests_to_run:
+        path = t.working_directory + f"\\{t.test_folder_name}\\" + file_name
         data = t.read_file_test(path)
-        data = data[1:]  # this seems to be GUI data
+        gui_bounds = data.pop(0)  # this seems to be GUI data
         result_near = t.run_test_nearest(data, reverse_inp=True)
         result_small = t.run_test_smallest(data, reverse_inp=True)
         dist_near = truncate(result_near[0], 4)
@@ -108,148 +146,81 @@ if __name__ == "__main__":
 
         print("test input:", data)
         print('near out:  ', result_near[1].__str__())
-        print('\t\t   ', dist_near, result_near[1].length)
+        print('\t\t   ', dist_near, result_near[1].length, result_near[2])
         print('small out: ', result_small[1].__str__())
-        print('\t\t   ', dist_small, result_small[1].length)
+        print('\t\t   ', dist_small, result_small[1].length, result_small[2])
         print('\t\t    ' + '-' * 32)
 
+    x_list = list()
+    y_list = list()
+    all_data = dict(near_data=[], small_data=[])
+
+    plot_near_data = ast.literal_eval(result_near[1].__str__())
+    plot_small_data = ast.literal_eval(result_small[1].__str__())
+
+    for x, y in plot_near_data:
+        x_list.append(x)
+        y_list.append(y)
+    all_data['near_data'].append((x_list, y_list))
+
+    x_list.clear()
+    y_list.clear()
+    for x, y in plot_small_data:
+        x_list.append(x)
+        y_list.append(y)
+    all_data['small_data'].append((x_list, y_list))
+
+    # x_list += [x_list[0], x_list[-1]]
+    # y_list += [y_list[0], y_list[-1]]
+
+    fig = go.Figure(
+        go.Scatter(x=x_list, y=y_list, mode='lines'),
+    )
+    # connect first<->last
+    fig.add_trace(
+        go.Scatter(x=[x_list[0], x_list[-1]], y=[y_list[0], y_list[-1]], mode='lines'),
+    )
+
+    fig.update_layout(
+        # width=gui_bounds[0] + gui_bounds[0],
+        # height=gui_bounds[1] + gui_bounds[1],
+        showlegend=False,
+        font_family="Courier New",
+        font_color="blue",
+        title_font_family="Times New Roman",
+        title_font_color="red",
+        legend_title_font_color="green",
+        yaxis=dict(scaleanchor="x", scaleratio=1),
+        dragmode=False,
+        updatemenus=[
+            dict(
+                type="buttons",
+                direction="left",
+                buttons=list([
+                    dict(
+                        label="Points Off",
+                        method="restyle",
+                        args=["mode", ["lines"]]
+                    ),
+                    dict(
+                        label="Points On",
+                        method="restyle",
+                        args=["mode", ["lines+markers"]]
+                    )
+                ]),
+                pad={"r": 10, "t": 10},
+                showactive=True,
+                x=0.11,
+                xanchor="center",
+                y=1.1,
+                yanchor="top"
+            ),
+        ],
+    )
+
+    config = {'displayModeBar': False,
+              'scrollZoom': True,
+              }
+    fig.show(config=config)
 
 
-
-
-
-
-
-
-fig = go.Figure(go.Scattermapbox(
-    mode = "markers+lines",
-    lon = [10, 20, 30],
-    lat = [10, 20,30],
-    marker = {'size': 10}))
-
-fig.add_trace(go.Scattermapbox(
-    mode = "markers+lines",
-    lon = [-50, -60,40],
-    lat = [30, 10, -20],
-    marker = {'size': 10}))
-
-fig.update_layout(
-    margin ={'l':0,'t':0,'b':0,'r':0},
-    mapbox = {
-        'center': {'lon': 10, 'lat': 10},
-        'style': "stamen-terrain",
-        'center': {'lon': -20, 'lat': -20},
-        'zoom': 1})
-
-fig.show()
-
-
-
-
-# class Example(Frame):
-#
-#     def __init__(self, data):
-#         super().__init__()
-#         self.data = data
-#         self.initUI(self.data)
-#
-#
-#
-#     def initUI(self, data):
-#         # self.master.title("Graph")
-#         self.pack(fill=BOTH, expand=1)
-#         canvas = Canvas(self)
-#
-#         # x = 100
-#         # y = 100
-#         size = 10
-#         # canvas.create_oval(x, y, x, y, width=100, fill='black')
-#         # self.data = [(100, 100)]
-#         for each_point in self.data:
-#             x = each_point[0]
-#             y = each_point[1]
-#             canvas.create_rectangle([x, y, x, y], width=size, fill='black')
-#
-#         canvas.pack(fill=BOTH, expand=1)
-#
-#
-#
-# def main():
-#
-#     t = Test()
-#     file_num = 2
-#     data = t.read_file_test(t.working_directory + f"/{t.test_folder_name}/" + t.test_file_names[file_num])
-#     data = data[1:]
-#     print(data)
-#     t.test_nearest(data)
-#
-#     root = Tk()
-#     ex = Example(data)
-#     root.geometry("400x250+300+300")
-#     root.mainloop()
-#
-#
-# if __name__ == '__main__':
-#     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-# to check output length: len(ast.literal_eval(result[1].__str__()))
-
-
-# heuristic_fn_names = ["insert_nearest", "insert_smallest"]  # heuristic method names
-# fns = {k: v for k, v in Tour.__dict__.items() if k in heuristic_fn_names}
-
-
-
-
-
-# test_cases: list[dict[str:list[tuple]]] = \
-#     [
-#         {
-#             "tsp2.txt":
-#             [
-#                 (500, 500),  # size of plane (>= bounds)
-#                 (200.0, 400.0),
-#                 (300.0, 100.0),
-#             ]
-#         },
-#
-#         {
-#             "tsp3.txt":
-#             [
-#                 (500, 500),
-#                 (200.0, 400.0),
-#                 (300.0, 100.0),
-#                 (100.0, 100.0),
-#             ]
-#         },
-#
-#         {
-#             "tsp10.txt":
-#             [
-#                 (600, 600),
-#                 (110.0, 225.0),
-#                 (161.0, 280.0),
-#                 (325.0, 554.0),
-#                 (490.0, 285.0),
-#                 (157.0, 443.0),
-#                 (283.0, 379.0),
-#                 (397.0, 566.0),
-#                 (306.0, 360.0),
-#                 (343.0, 110.0),
-#                 (552.0, 199.0),
-#             ]
-#         },
-#
-#     ]
