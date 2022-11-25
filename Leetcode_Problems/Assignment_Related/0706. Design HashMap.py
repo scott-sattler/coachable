28c2e5416da5eb9978b56f60196deeaef3b80cb7
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2022-11-25 14:02:21 -0600

    template; no testing

--- Leetcode_Problems/Assignment_Related/0706. Design HashMap.py
+++ Leetcode_Problems/Assignment_Related/0706. Design HashMap.py
@@ -38,15 +38,21 @@ Constraints:
 
 class MyHashMap:
 
+    # MyHashMap() initializes the object with an empty map.
     def __init__(self):
         pass
 
+    # void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map,
+    # update the corresponding value.
     def put(self, key: int, value: int) -> None:
         pass
 
+    # int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for
+    # the key.
     def get(self, key: int) -> int:
         pass
 
+    # void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
     def remove(self, key: int) -> None:
         pass
 
