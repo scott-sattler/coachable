870597bff0a686c6af5fb71cc49b98abfa78e5aa
scottsattler <scott.sattler.dev@gmail.com>
2023-02-09 13:37:46 -0800

    solution; no followup

--- Leetcode_Problems/844. Backspace String Compare.py
+++ Leetcode_Problems/844. Backspace String Compare.py
@@ -2,7 +2,8 @@
 844. Backspace String Compare
 Easy
 
-Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a
+backspace character.
 
 Note that after backspacing an empty text, the text will continue empty.
 
@@ -34,32 +35,79 @@ Follow up: Can you solve it in O(n) time and O(1) space?
 
 
 class Solution:
-    # first attempt:
+    # second attempt: followup O(1) auxiliary
     # time complexity:
     # space complexity (auxiliary):
     def backspaceCompare(self, s: str, t: str) -> bool:  # noqa: naming convention
-        pass
+        raise NotImplemented
+
+    # first attempt:
+    # time complexity: O(2s + 2t) -> O(s + t)
+    # space complexity (auxiliary): O(2s + 2t) -> O(s + t) auxiliary
+    def first_attempt_backspaceCompare(self, s: str, t: str) -> bool:  # noqa: naming convention
+        stack_s = list()
+        stack_t = list()
+
+        # construct backspaced strings (stacks)
+        for char in s:
+            if char != "#":
+                stack_s.append(char)
+            elif len(stack_s) > 0:
+                stack_s.pop()
+
+        for char in t:
+            if char != "#":
+                stack_t.append(char)
+            elif len(stack_t) > 0:
+                stack_t.pop()
+
+        return True if stack_s == stack_t else False
 
 
 test_cases = [
-    [
-        "a",
-        "b"
-    ],  # False
-    [
-        "ab#c",
-        "ad#c"
-    ],  # True
+    # # included
+    # [
+    #     "ab#c",
+    #     "ad#c",
+    #     True
+    # ],
     [
         "ab##",
-        "c#d#"
-    ],  # True
+        "c#d#",
+        True
+    ],
     [
         "a#c",
-        "b"
-    ],  # False
+        "b",
+        False
+    ],
+
+    # additional
+    [
+        "a",
+        "b",
+        False
+    ],
+    [
+        "a#c######",
+        "b#",
+        True
+    ],
+
+    # failed
+    [
+        "bxj##tw",
+        "bxj###tw",
+        False
+    ],
+    [
+        "ab##",
+        "c#d#",
+        True
+    ],
 
 ]
 
-for s, t in test_cases:
-    print((s, t), Solution().backspaceCompare(s, t))
+print("(s, t), ans, Solution().backspaceCompare(s, t)")
+for s, t, ans in test_cases:
+    print((s, t), ans, Solution().backspaceCompare(s, t))
