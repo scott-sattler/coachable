e574a254b35670af09013d1dafdeedc95c3c2c77
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2023-02-28 17:53:46 -0600

    initial commit

---
+++ DS&A Crash Course/DS&A Week 8/DS&A Final Exam Study Guide.py
@@ -0,0 +1,68 @@
+"""
+Week 1: Python Fundamentals and Object-Oriented Programming
+
+    How method calls work
+
+    Pass by reference vs value
+
+    Classes and instances
+
+    OOD best practices
+
+Week 2: Runtime Analysis, Linked Lists, Stack/Queue
+
+    How to analyze a block of code for runtime
+
+    How to analyze a block of code for space complexity
+
+    Linked list concepts (insert front/back, detect cycle, singly/doubly linked, etc.)
+
+    Stack and queue concepts and when to use one over the other
+
+Week 3: Hashmaps, Recursion
+
+    How hashmaps work under the hood (i.e. list + linear probing)
+
+    What makes a good hash function
+
+    Recursion basics
+
+    Binary search
+
+Week 4: Strings, Sorting Algorithms
+
+    String techniques using in coding interviews
+
+    String concepts (palindrome, anagram, subsequence, etc.) and related algorithms
+
+    All the different sorting algorithms
+
+    How to sort different types (int, tuple, etc.) in Python
+
+Week 5: Trees and Tries
+
+    Tree traversals and when to use each
+
+    What is a trie
+
+    How to build a trie in Python
+
+Week 6: Graphs
+
+    Graph representations
+
+    Shortest path algorithms
+
+    Ordering algorithms
+
+    Directed vs undirected graphs
+
+    Cycle detection
+
+Week 7: Dynamic Programming
+
+    Relationship between Recursion and Dynamic Programming
+
+    Top-Down vs Bottom-Up
+
+"""
