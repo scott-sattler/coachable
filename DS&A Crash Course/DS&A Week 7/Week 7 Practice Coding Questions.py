2850abbf7ea92309d6d0625ea5338879e6c4cc33
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2023-03-06 13:05:55 -0600

    partial solution

--- DS&A Crash Course/DS&A Week 7/Week 7 Practice Coding Questions.py
+++ DS&A Crash Course/DS&A Week 7/Week 7 Practice Coding Questions.py
@@ -24,11 +24,63 @@ Explanation: From the top-left corner, there are a total of 3 ways to reach the 
 '''
 
 
+# extended learning: brute force solution
+# time: O(2^(m + n))
+# auxiliary O(m + n)
+def unique_paths(m: int, n: int) -> int:
+    return _unique_paths(0, 0, m, n)
+
+
+def _unique_paths(i: int, j: int, m: int, n: int) -> int:
+    if i > m or j > n:
+        return 0
+    if i == (m - 1) and j == (n - 1):
+        return 1
+    return _unique_paths(i + 1, j, m, n) + _unique_paths(i, j + 1, m, n)
+
+
+###############
+
+
 # Question 1. Solve the above problem using Top Down Dynamic Programming
 def unique_paths_top_down(m: int, n: int) -> int:
-    pass
+    # recursion
+    # memoization
+    memo = {(0, 0): 0}
+    return -1
 
 
 # Question 2. Solve the above problem using Bottom Up Dynamic Programming
+# time: O(m + n)
+# auxiliary O(m * x)
 def unique_paths_bottom_up(m: int, n: int) -> int:
-    pass
+    # iteration
+    # tabulation
+    table = [[0 for _ in range(n)] for _ in range(m)]
+
+    # first row
+    for j in range(n):
+        table[0][j] = 1
+
+    # first column
+    for i in range(m):
+        table[i][0] = 1
+
+    # fill table
+    for i in range(1, m):
+        for j in range(1, n):
+            table[i][j] = table[i - 1][j] + table[i][j - 1]
+
+    return table[m - 1][n - 1]
+
+
+
+
+
+
+
+for i in range(1, 5):
+    ans = unique_paths_bottom_up(i, i)
+    print(i, ans)
+
+    print(i, unique_paths(i, i))
