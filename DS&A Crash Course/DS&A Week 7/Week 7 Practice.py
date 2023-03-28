8a0f3fe3c596d6a2dcfe76c96b5874dbdfceee38
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2023-03-28 18:39:44 -0500

    checkpoint

--- DS&A Crash Course/DS&A Week 7/Week 7 Practice.py
+++ DS&A Crash Course/DS&A Week 7/Week 7 Practice.py
@@ -29,12 +29,12 @@ Free Response Questions
 
     5. Oftentimes, the answer  f(n) may only require the result from f(n-1). What kind of space/memory optimization can
     we do if this is the case? If it helps, you can give an example of a specific problem.
-***
+        * no response to inquiry *
         memoization/tabulation
 
     6. What if we had a f(r, c) that relied only on f(r-1, c) for some arbitrary c? What kind of space/memory
     optimization can we do if this is the case? If it helps, you can give an example of a specific problem.
-***
+        * no response to inquiry *
         memoization/tabulation
 
     7. Fibonacci is one example of a 1-dimensional recurrence relation optimized with dynamic programming. Identify and
@@ -43,67 +43,178 @@ Free Response Questions
         matrices: (e.g.) min/max paths; 2D combinations
         counting: (e.g.) knapsack problem; coin change problem
 
-        attempt to coherently categorize:
         optimization min/max graphs
         optimization min/max strings
         target combination/permutation
-        other?
 
     8. Is this statement true or false?
     "Dynamic programming only helps problems that have a brute-force recursive solution."
         false
 
 Dynamic Programming and Recurrence Examples
-
-    Each of the below problems can be solved with recursion. Please  answer the following for each one.
-
+    Each of the below problems can be solved with recursion. Please answer the following for each one.
     1. Unit Tests. Identify the solution cases up to n = 4. For 2D, do all cases up to (3,3)
-
     2. Recurrence Relation.  Identify the recurrence relationship and base cases. Explain why they are true by giving a
     qualitative explanation in plain English.
-
     3. Bottom Up. Compute a small example using a bottom-up (n = 6, m = 6)
-
-    4. Top Down Approach. Compute a small example using a top-down approach with memorization. (n=6,m=6)
-
+    4. Top Down Approach. Compute a small example using a top-down approach with memoization. (n=6,m=6)
     5. Complexity Analysis. Identify the optimal runtime and space complexity.
-
     6. Is DP worth it? Does dynamic programming improve the runtime compared to a recursive approach?
 
 Problems That Can Be Solved with Recursion
 
     1. Number of paths up a staircase of length N where you take 1 or 2 steps each time. Example provided.
-        1. 0 stairs 0 solutions; 1 1; 2 2; 3 3
-        2. T(n) = c + T(n - 1) + T(n - 2); if remaining_stairs == 0: return 1;
-           recurrence: at each step, you can choose either 1 or two steps. the recursive stack acts as tree nodes
-           base case: when no choices can be made (no stairs remain), you count a unique outcome
-        3. 13 - used tree by hand
-        code at bottom
-        def steps(n):
-            if n < 1:
-                return 1 * (n + 1)
-            return steps(n - 1) + steps(n - 2)
+        1. Unit Tests. Identify the solution cases up to n = 4. For 2D, do all cases up to (3,3)
+            0 stairs 0 solutions; 1 1; 2 2; 3 3
 
-        4. 13
-        5. O(2^n)
+        2. Recurrence Relation.  Identify the recurrence relationship and base cases. Explain why they are true by
+        giving a qualitative explanation in plain English.
+            T(n) = c + T(n - 1) + T(n - 2); if remaining_stairs == 0: return 1;
+            recurrence: at each step, you can choose either 1 or two steps. the recursive stack acts as tree nodes
+            base case: when no choices can be made (no stairs remain), you count a unique outcome
 
+        3. Bottom Up. Compute a small example using a bottom-up (n = 6, m = 6)
+            13; algorithm at bottom below
+            [[2, 2, 1, 1], [2, 2, 2], [2, 1, 2, 1], [2, 1, 1, 1, 1], [2, 1, 1, 2],
+            [1, 2, 2, 1], [1, 2, 1, 1, 1], [1, 2, 1, 2], [1, 1, 2, 1, 1], [1, 1, 2, 2],
+            [1, 1, 1, 2, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 2]]
+
+        4. Top Down Approach. Compute a small example using a top-down approach with memoization. (n=6,m=6)
+            13; algorithm at bottom below
+            the memo is still created in a 'bottom up' fashion
+                the callstack contains a stack of references with the base case at the top, hence, 'bottom up'
+
+        5. Complexity Analysis. Identify the optimal runtime and space complexity.
+            optimal run/space, Ω(n), is the closed form with Ω(n) time-complexity and Ω(1) auxiliary space
+            DP: O(n) runtime; O(n + n) -> O(n) aux space (callstack + memo)
+            naive recursive: O(2^n) runtime; O(n) aux space (callstack)
+
+        6. Is DP worth it? Does dynamic programming improve the runtime compared to a recursive approach?
+            yes; yes. DP incurs a space penalty vs naive recursion
 
     2. Computing the number of permutations of [1-n] i.e. [1,2,3,4,5,...n-1,n]
+        1. Unit Tests. Identify the solution cases up to n = 4. For 2D, do all cases up to (3,3)
+            there are n! permutations of n elements
+            0 -> 1; 1 1; 2 2; 3 6; 4 24
+
+        2. Recurrence Relation.  Identify the recurrence relationship and base cases. Explain why they are true by
+        giving a qualitative explanation in plain English.
+            T(n) = n * T(n - 1); if n == 0: return 1
+            recurrence: the 1st element can be in (n - 0) positions, the 2nd element (n - 1) positions, 3rd (n - 2)...
+                        each element can take each position, the number of ways all other elements can be dif. ordered
+                        while it is in that position
+                        [1, 2, 3, 4], [1, 3, 2, 4], ...
+                        but that just describes factorial (permutations), to get the actual recurrence relation, I just
+                        used existing knowledge of recurrence relations, vaguely recalling I had seen n! before
+            base case: factorials cannot be negative (cannot have negative orderings); 0 elements contain 1 ordering
+
+        3. Bottom Up. Compute a small example using a bottom-up (n = 6, m = 6)
+            720; [1, 1, 2, 6... ]; algorithm at bottom below
+
+        4. Top Down Approach. Compute a small example using a top-down approach with memoization. (n=6,m=6)
+            720; algorithm at bottom below
+
+        5. Complexity Analysis. Identify the optimal runtime and space complexity.
+            optimal run/space, Ω(n), is the closed form n! with Ω(1) or Ω(n) time-complexity and Ω(1) auxiliary space
+                Ω(1) or Ω(n) depends on multiplication implementation
+            DP memo: O(n) runtime; O(n + n) -> O(n) aux space (callstack + memo)
+            DP table: O(n) runtime; O(1) -> O(1) aux space (optimized table; O(n) unoptimized table)
+            naive recursive: O(n); O(n)
+
+        6. Is DP worth it? Does dynamic programming improve the runtime compared to a recursive approach?
+            not really (?); asymptotically identical, practically, probably worse (?)
 
     3. Unique Paths: The number of paths from the top left corner of a grid to the bottom right corner when moving only
     down and to the right.
+        1. Unit Tests. Identify the solution cases up to n = 4. For 2D, do all cases up to (3,3)
+
+        2. Recurrence Relation.  Identify the recurrence relationship and base cases. Explain why they are true by
+        giving a qualitative explanation in plain English.
+
+        3. Bottom Up. Compute a small example using a bottom-up (n = 6, m = 6)
+
+        4. Top Down Approach. Compute a small example using a top-down approach with memoization. (n=6,m=6)
+
+        5. Complexity Analysis. Identify the optimal runtime and space complexity.
+
+        6. Is DP worth it? Does dynamic programming improve the runtime compared to a recursive approach?
 
     4. Given a 2xN grid, how many different ways can you fill the gird with 2x1 dominoes?
+        1. Unit Tests. Identify the solution cases up to n = 4. For 2D, do all cases up to (3,3)
+
+        2. Recurrence Relation.  Identify the recurrence relationship and base cases. Explain why they are true by
+        giving a qualitative explanation in plain English.
+
+        3. Bottom Up. Compute a small example using a bottom-up (n = 6, m = 6)
+
+        4. Top Down Approach. Compute a small example using a top-down approach with memoization. (n=6,m=6)
+
+        5. Complexity Analysis. Identify the optimal runtime and space complexity.
+
+        6. Is DP worth it? Does dynamic programming improve the runtime compared to a recursive approach?
 
     5. Given a set A = {1,2,3,...,N} calculate the number of possible subsets of A.
+        1. Unit Tests. Identify the solution cases up to n = 4. For 2D, do all cases up to (3,3)
+            there are 2^n subsets within a set
+            0 -> 1; 1 2; 2 4; 3 9; 4 16
+
+        2. Recurrence Relation.  Identify the recurrence relationship and base cases. Explain why they are true by
+        giving a qualitative explanation in plain English.
+
+
+        3. Bottom Up. Compute a small example using a bottom-up (n = 6, m = 6)
+
+        4. Top Down Approach. Compute a small example using a top-down approach with memoization. (n=6,m=6)
+
+        5. Complexity Analysis. Identify the optimal runtime and space complexity.
+
+        6. Is DP worth it? Does dynamic programming improve the runtime compared to a recursive approach?
 
     6. Count the number of functions from {1,2,3,...,N} to a set of size {1,2,3,...,M}. Here is an additional
     explanation of functions.
+        1. Unit Tests. Identify the solution cases up to n = 4. For 2D, do all cases up to (3,3)
+
+        2. Recurrence Relation.  Identify the recurrence relationship and base cases. Explain why they are true by
+        giving a qualitative explanation in plain English.
+
+        3. Bottom Up. Compute a small example using a bottom-up (n = 6, m = 6)
+
+        4. Top Down Approach. Compute a small example using a top-down approach with memoization. (n=6,m=6)
+
+        5. Complexity Analysis. Identify the optimal runtime and space complexity.
+
+        6. Is DP worth it? Does dynamic programming improve the runtime compared to a recursive approach?
 
     7. Extension of problem 6. A function has a fixed point if f(x) = x for any x in the domain of f . How many
     functions are there from {1,2,3,...,N} to {1,2,3,...,M} without any fixed points?
+        1. Unit Tests. Identify the solution cases up to n = 4. For 2D, do all cases up to (3,3)
+
+        2. Recurrence Relation.  Identify the recurrence relationship and base cases. Explain why they are true by
+        giving a qualitative explanation in plain English.
+
+        3. Bottom Up. Compute a small example using a bottom-up (n = 6, m = 6)
+
+        4. Top Down Approach. Compute a small example using a top-down approach with memoization. (n=6,m=6)
+
+        5. Complexity Analysis. Identify the optimal runtime and space complexity.
+
+        6. Is DP worth it? Does dynamic programming improve the runtime compared to a recursive approach?
 
     8. Given a 4xN grid, how many different ways can you fill the grid with 2x1 dominoes?
+        1. Unit Tests. Identify the solution cases up to n = 4. For 2D, do all cases up to (3,3)
+            0 1; 1 1; 2 5; 3 11; 4 36
+
+        2. Recurrence Relation.  Identify the recurrence relationship and base cases. Explain why they are true by
+        giving a qualitative explanation in plain English.
+
+        3. Bottom Up. Compute a small example using a bottom-up (n = 6, m = 6)
+
+        4. Top Down Approach. Compute a small example using a top-down approach with memoization. (n=6,m=6)
+
+        5. Complexity Analysis. Identify the optimal runtime and space complexity.
+
+        6. Is DP worth it? Does dynamic programming improve the runtime compared to a recursive approach?
+
 
 OPTIONAL: Binary Tree Recursive Challenge Problems
 
@@ -168,6 +279,7 @@ OPTIONAL: Binary Tree Recursive Challenge Problems
 """
 
 
+# recursive solution
 def steps(n):
     if n < 1:
         return 1 * (n + 1)
@@ -178,30 +290,52 @@ ans = steps(6)
 print(ans)
 
 
+# steps inverse
 # 6 7
 # % 5 -> 1 2
 # % 6 -> 0 1
-#
-def steps_inv(n):
-    if n > 5:
-        return ((n % 6) - 1) ** 2  # **2 <-> abs()
-    return steps_inv(n + 1) + steps_inv(n + 2)
+def steps_inv(m, n):
+    if m > n:
+        return ((m % n) - 1) ** 2  # **2 <-> abs()
+    return steps_inv(m + 1, n) + steps_inv(m + 2, n)
 
 
-ans = steps_inv(6)
+ans = steps_inv(0, 6)
 print(ans)
 
 
-def steps_b_up_no_cache(n):
-    pass
+# ##### 1. Number of paths up a staircase of length N where you take 1 or 2 steps each time. Example provided.
+print('1. staircase...')
 
 
-ans = steps_b_up_no_cache(6)
+# 3. Bottom Up. Compute a small example using a bottom-up (n = 6, m = 6)
+def steps_b_up_tab(n):
+    agenda = [[]]
+    solutions = 0
+    # solutions = []
+    while agenda:
+        nxt = agenda.pop()
+        if sum(nxt) + 2 == n:
+            solutions += 2
+            # solutions += [next + [1, 1], next + [2]]
+        elif sum(nxt) + 1 == n:
+            solutions += 1
+            # solutions += [next + [1]]
+        elif sum(nxt) + 2 < n:
+            agenda.append(nxt + [1])
+            agenda.append(nxt + [2])
+        elif sum(nxt) + 1 < n:
+            agenda.append(nxt + [1])
+
+    return solutions
+
+
+ans = steps_b_up_tab(6)
 print(ans)
 
 
-
-def steps_t_down_memo(n, memo):
+# 4. Top Down Approach. Compute a small example using a top-down approach with memoization. (n=6,m=6)
+def steps_t_down_memo(n):
     memo = dict()
     return _steps_t_down_memo(n, memo)
 
@@ -210,8 +344,70 @@ def _steps_t_down_memo(n, memo):
     if n < 1:
         return 1 * (n + 1)
 
-    pass
+    if n not in memo:
+        memo[n] = _steps_t_down_memo(n - 1, memo) + _steps_t_down_memo(n - 2, memo)
+
+    return memo[n]
 
 
+ans = steps_t_down_memo(6)
+print(ans)
 
+
+# ##### 2. Computing the number of permutations of [1-n] i.e. [1,2,3,4,5,...n-1,n]
+print('2. permutations...')
+
+
+# 3. Bottom Up. Compute a small example using a bottom-up (n = 6, m = 6)
+def perm_b_up(n):
+    tab = [1] * n
+    for i in range(1, n):
+        tab[i] = (i + 1) * tab[i - 1]
+    return tab[-1]
+
+
+ans = perm_b_up(6)
+print(ans)
+
+
+def perm_b_up_optimized(n):
+    tab = [1, 1]
+    for i in range(1, n):
+        tab[0] = (i + 1) * tab[1]
+        tab[1] = tab[0]
+    return tab[1]
+
+
+ans = perm_b_up_optimized(6)
+print(ans)
+
+
+# 4. Top Down Approach. Compute a small example using a top-down approach with memoization. (n=6,m=6)
+def perm_t_down(n):
+    memo = dict()
+    return _perm_t_down(n, memo)
+
+
+def _perm_t_down(n, memo):
+    if n == 0:
+        return 1
+    if n not in memo:
+        memo[n] = n * _perm_t_down(n - 1, memo)
+
+    return memo[n]
+
+
+ans = perm_t_down(6)
+print(ans)
+
+
+# ##### 3. Unique Paths: The number of paths from the top left corner of a grid to the bottom right corner when moving
+#                        only down and to the right.
+print('3. unique paths...')
+
+
+# 3. Bottom Up. Compute a small example using a bottom-up (n = 6, m = 6)
+
+
+# 4. Top Down Approach. Compute a small example using a top-down approach with memoization. (n=6,m=6)
 
