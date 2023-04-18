3907b1ccd49ae4026f982f335439acfdb2b981ec
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2023-04-18 03:41:00 -0500

    solved

--- DS&A Crash Course/Week 2/Week 2 Coding Exercises (Traveling Salesman Problem).py
+++ DS&A Crash Course/Week 2/Week 2 Coding Exercises (Traveling Salesman Problem).py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from __future__ import annotations
+
 
 # (2D point data type to model points in the plane.)
 class Point:
@@ -16,8 +18,15 @@ class Point:
 
     # return Euclidean distance between the two points
     def distance_to(self, that: Point) -> float:
-        return (self.x**2 + self.y**2)**(1/2)
+        return ((that.x - self.x) ** 2 + (that.y - self.y) ** 2) ** (1 / 2)
 
+
+a = Point(0, 0)
+b = Point(0, 2)
+ans = 2.0
+print(a.distance_to(b), ans)
+
+# from __future__ import annotations
 
 '''
 Tour class which is a collection of ordered points.
@@ -29,6 +38,7 @@ and insert a new point using either of the two heuristics.
 The constructor creates an empty tour.
 '''
 
+
 # from point import Point
 
 
@@ -37,7 +47,6 @@ class Node:
     def __init__(self, point):
         # This node's point
         self.point = point
-
         # The next node
         self.next = None
 
@@ -46,21 +55,40 @@ class Tour:
     # Creates an empty tour
     # Initialize any instance variables you think are needed.
     def __init__(self):
-        pass
+        self.head = None
+        self.nodes = 0
 
     # Returns string representation of the Tour.
     # Should output a list of all points on the Tour.
     def __str__(self):
-        pass
+        str_list = []
+        pointer = self.head
+        while pointer:
+            str_list.append(pointer.point.__str__)
+            pointer = pointer.next
+        return ' '.join(str_list)
 
     # return the number of points on tour
     # Hint: You should not have to iterate through the entire Tour to get the size.
     def size(self):
-        pass
+        return self.nodes
 
-    # Computers and returns the distance of entire tour
+    # Computes and returns the distance of entire tour
     def distance(self):
-        pass
+        dist = 0
+        if not self.head:
+            return dist
+
+        pointer = self.head
+        while pointer.next:
+
+            dist += pointer.point.distance_to(pointer.next.point)
+            pointer = pointer.next
+
+        if self.size() > 1:
+            dist += pointer.point.distance_to(self.head.point)
+
+        return dist
 
     # Helper function to insert a new point p into the Tour after a previous Node prev.
     # Example if the tour is a -> b -> c -> d
@@ -69,12 +97,256 @@ class Tour:
     # You can use this helper function in the insertNearest and insertSmallest
     # once you find the point you should insert p after.
     def _insert_at(self, p, prev: Node):
-        pass
+        pointer = self.head
+        new_node = Node(p)
+        self.nodes += 1
 
-    # Insert a new Point p to the Tour using neearest neighbor heuristic
+        if not pointer:
+            self.head = new_node
+            return
+
+        while pointer is not prev:
+            pointer = pointer.next
+
+        new_node.next = pointer.next
+        pointer.next = new_node
+
+    # Insert a new Point p to the Tour using nearest neighbor heuristic
     def insert_nearest(self, p):
-        pass
+        # nearest neighbor: min(distance between current and all other nodes)
+        pointer = self.head
+        prev_node = pointer
+        nearest = float('inf')
+
+        while pointer:
+            dist = abs(pointer.point.distance_to(p))
+            if dist < nearest:
+                nearest = dist
+                prev_node = pointer
+            pointer = pointer.next
+
+        self._insert_at(p, prev_node)
+
+    # # Insert a new Point p to the Tour using smallest increase heuristic
+    # def insert_smallest(self, p):
+    #     # smallest increase heuristic: min(insert node between every other node)
+    #     new_node = Node(p)
+    #     pointer = self.head
+    #     smallest = float('inf')
+    #     prev = pointer
+    #
+    #     while pointer:
+    #         new_node.next = pointer.next
+    #         pointer.next = new_node
+    #
+    #         new_dist = self.distance()
+    #         if new_dist < smallest:
+    #             smallest = new_dist
+    #             prev = pointer
+    #
+    #         pointer.next = new_node.next
+    #         pointer = pointer.next
+    #
+    #     # restore smallest increase
+    #     self._insert_at(p, prev)
 
     # Insert a new Point p to the Tour using smallest increase heuristic
     def insert_smallest(self, p):
-        pass
+        # smallest increase heuristic: min(insert node between every other node)
+
+        # performant solution:
+        # subtract distance from pointer_node to pointer_node.next
+        # add distance from pointer_node to new_node
+        # add distance from new_node to pointer_node.next
+
+        p1 = self.head
+        p2 = p1.next if self.head else None
+        min_node = self.head
+        min_diff = float('inf')
+
+        while p1 and p2:
+            diff = p1.point.distance_to(p2.point)
+            add_a = p1.point.distance_to(p)
+            add_b = p.distance_to(p2.point)
+
+            if (add_a + add_b - diff) < min_diff:
+                min_node = p1
+                min_diff = (add_a + add_b - diff)
+
+            p1 = p1.next
+            p2 = p2.next if p2.next else self.head  # loop it
+
+        # restore smallest increase
+        self._insert_at(p, min_node)
+
+
+
+# ##############################
+# ########## testing ###########
+# ##############################
+
+
+import unittest
+
+
+# from __future__ import annotations
+# from point import Point
+# from tour import Tour
+
+
+class TestPoint(unittest.TestCase):
+
+    def test_string(self):
+        a = Point(1, 2)
+        self.assertEqual(str(a), '(1,2)')
+
+    def test_distance(self):
+        a = Point(0, 0)
+        b = Point(0, 2)
+        self.assertEqual(a.distance_to(b), 2.0)
+
+        c = Point(4, 5)
+        d = Point(1, 1)
+        self.assertEqual(c.distance_to(d), 5.0)
+
+        e = Point(1, 7)
+        f = Point(13, 2)
+        self.assertEqual(e.distance_to(f), 13.0)
+
+
+def read_point_file(filename: str):
+    point_list = []
+    with open(filename) as f:
+        lines = f.readlines()
+        for line in lines:
+            row = line.split()
+            x = float(row[0])
+            y = float(row[1])
+            p = Point(x, y)
+            point_list.append(p)
+    return point_list
+
+
+class TestTour(unittest.TestCase):
+    file_location = './test_data'
+    tsp_2 = read_point_file(file_location + '/tsp2.txt')
+    tsp_3 = read_point_file(file_location + '/tsp3.txt')
+    tsp_10 = read_point_file(file_location + '/tsp10.txt')
+    tsp_100 = read_point_file(file_location + '/tsp100.txt')
+    tsp_1000 = read_point_file(file_location + '/tsp1000.txt')
+
+    def test_tour_simple_example(self):
+        coordinates = [
+            (0, 0),
+            (3, 0),
+            (0, 4)
+        ]
+
+        tour = Tour()
+
+        for x, y in coordinates:
+            p = Point(x, y)
+            tour.insert_nearest(p)
+
+        self.assertEqual(tour.size(), 3)
+        self.assertEqual(tour.distance(), 12.0)
+
+    def test_duplicates(self):
+        coordinates = [
+            (0, 0),
+            (3, 0),
+            (0, 4),
+            (0, 0),
+            (0, 0),
+            (0, 4)
+        ]
+
+        tour = Tour()
+
+        for x, y in coordinates:
+            p = Point(x, y)
+            tour.insert_smallest(p)
+
+        self.assertEqual(tour.size(), 6)
+        self.assertEqual(tour.distance(), 12.0)
+
+    def test_empty_tour(self):
+        tour = Tour()
+        self.assertEqual(tour.size(), 0)
+        self.assertEqual(tour.distance(), 0)
+
+    def test_tsp_2_insert_smallest(self):
+        smallest_tour = Tour()
+        for p in self.tsp_2:
+            smallest_tour.insert_smallest(p)
+        self.assertAlmostEqual(smallest_tour.distance(), 565.68542, places=2)
+        self.assertEqual(smallest_tour.size(), 2)
+
+    def test_tsp_3_insert_smallest(self):
+        smallest_tour = Tour()
+        for p in self.tsp_3:
+            smallest_tour.insert_smallest(p)
+        self.assertAlmostEqual(smallest_tour.distance(), 832.4555320336758, places=2)
+        self.assertEqual(smallest_tour.size(), 3)
+
+    def test_tsp_10_insert_smallest(self):
+        smallest_tour = Tour()
+        for p in self.tsp_10:
+            smallest_tour.insert_smallest(p)
+        self.assertAlmostEqual(smallest_tour.distance(), 1655.7461857661865, places=2)
+        self.assertEqual(smallest_tour.size(), 10)
+
+    def test_tsp_100_insert_smallest(self):
+        smallest_tour = Tour()
+        for p in self.tsp_100:
+            smallest_tour.insert_smallest(p)
+        self.assertAlmostEqual(smallest_tour.distance(), 4887.219040311983, places=2)
+        self.assertEqual(smallest_tour.size(), 100)
+
+    def test_tsp_1000_insert_smallest(self):
+        smallest_tour = Tour()
+        for p in self.tsp_1000:
+            smallest_tour.insert_smallest(p)
+        self.assertAlmostEqual(smallest_tour.distance(), 17265.6282, places=2)
+        self.assertEqual(smallest_tour.size(), 1000)
+
+    def test_tsp_2_insert_nearest(self):
+        nearest_tour = Tour()
+        for p in self.tsp_2:
+            nearest_tour.insert_nearest(p)
+        self.assertAlmostEqual(nearest_tour.distance(), 565.68542, places=4)
+        self.assertEqual(nearest_tour.size(), 2)
+
+    def test_tsp_3_insert_nearest(self):
+        nearest_tour = Tour()
+        for p in self.tsp_3:
+            nearest_tour.insert_nearest(p)
+        self.assertAlmostEqual(nearest_tour.distance(), 832.4555320336758, places=4)
+        self.assertEqual(nearest_tour.size(), 3)
+
+    def test_tsp_10_insert_nearest(self):
+        nearest_tour = Tour()
+        for p in self.tsp_10:
+            nearest_tour.insert_nearest(p)
+        self.assertAlmostEqual(nearest_tour.distance(), 1566.1363051360363, places=4)
+        self.assertEqual(nearest_tour.size(), 10)
+
+    def test_tsp_100_insert_nearest(self):
+        nearest_tour = Tour()
+        for p in self.tsp_100:
+            nearest_tour.insert_nearest(p)
+        self.assertAlmostEqual(nearest_tour.distance(), 7389.929676351667, places=4)
+        self.assertEqual(nearest_tour.size(), 100)
+
+    def test_tsp_1000_insert_nearest(self):
+        nearest_tour = Tour()
+        for p in self.tsp_1000:
+            nearest_tour.insert_nearest(p)
+        self.assertAlmostEqual(nearest_tour.distance(), 27868.7106, places=4)
+        self.assertEqual(nearest_tour.size(), 1000)
+
+
+import pytest
+
+# do not modify this function call
+retcode = pytest.main(['-v'])
