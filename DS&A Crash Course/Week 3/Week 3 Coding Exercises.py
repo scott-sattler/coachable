3512c8f36ef5321b7ec1301c4871c5cd71422060
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2023-06-04 18:20:47 -0500

    first solution

--- DS&A Crash Course/Week 3/Week 3 Coding Exercises.py
+++ DS&A Crash Course/Week 3/Week 3 Coding Exercises.py
@@ -5,9 +5,55 @@ Return the list of numbers as a string separated by space using recursion
 '''
 
 
-def recurse(lst: list[int]) -> None:
+# more performant (perf + clarity ~?= 0); records data within itself via implicit data structure
+# O(2n) + O(n^2) -> O(n^2) time (n calls; n .join); O(2n) -> O(n) aux space (n call stack; n implicit structure)
+# First Attempt
+# not able to easily resolve pop() vs pop(0)
+def recurse_1(lst: list[int | str]) -> str:
+    if isinstance(lst[0], str):
+        return ' '.join(lst)
+
+    lst.append(str(lst.pop(0)))  # issue: pop() vs (pop(0) O(.5n(n+1)) -> O(n^2))
+
+    return recurse(lst)
+
+
+# Second Atteempt
+# clean; poor performance
+def recurse_2(lst: list[int]) -> str:
+    if len(lst) == 1:
+        return str(lst[0])
+
+    return recurse(lst[0:1]) + ' ' + recurse(lst[1:])
+
+
+# Third Attempt
+# O(n) time; O(1) aux space
+def recurse(lst: list[int | str]) -> str:
+    # base case
+    if isinstance(lst[0], str):
+        lst.pop()
+        return ' '.join(lst)
+
+    # create implicit data structure
+    if not isinstance(lst[-2], str):
+        lst[-1] = str(lst[-1])
+        lst.append(-2)
+
+    lst[lst[-1]] = str(lst[lst[-1]])
+    lst[-1] -= 1
+
+    return recurse(lst)
+
+
+# Fourth Attempt
+def recurse(lst: list[int | str]) -> str:
+    # keep return data in outermost fn call
     pass
 
+
+x = recurse([1, 2, 3, 4, 5])
+print(repr(x))
 
 '''
 2. Calculate the factorial of N iteratively and recursively
