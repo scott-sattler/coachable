f65ef8a4f0d2bf035cbb5251f79136f3da48779b
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2023-06-05 17:25:45 -0500

    completed w/ additional test cases

--- DS&A Crash Course/Week 3/Week 3 Coding Exercises.py
+++ DS&A Crash Course/Week 3/Week 3 Coding Exercises.py
@@ -17,7 +17,7 @@ def recurse_1(lst: list[int | str]) -> str:
     return recurse(lst)
 
 
-# Second Atteempt
+# Second Attempt
 # clean; poor performance
 def recurse_2(lst: list[int]) -> str:
     if len(lst) == 1:
@@ -77,24 +77,20 @@ Return -1 if number does not exist. Assume that the list is sorted.
 
 def find_index(lst: list[int], val: int) -> int:
     lo = 0
-    hi = len(lst)
+    hi = len(lst) - 1
 
-    while hi - lo > 1:
-        indx = lo + (hi - lo) // 2
+    while hi >= lo:
+        mid = lo + (hi - lo) // 2
 
-        if lst[indx] == val:
-            return indx
+        if lst[mid] == val:
+            return mid
 
-        if lst[indx] > val:
-            hi = indx
+        if lst[mid] > val:
+            hi = mid - 1
         else:  # go right
-            lo = indx
-
-    # todo: refactor/debug for inputs of size 1, 2
+            lo = mid + 1
 
     return -1
-
-
 
 
 '''
@@ -104,11 +100,30 @@ Return -1 if such a number does not exist. Assume that the list is sorted.
 
 
 def find_closest(lst: list[int], val: int) -> int:
-    pass
+    # biggest number less than or equal to the given value
+
+    lo = 0
+    hi = len(lst) - 1
+    lowest = -1
+
+    while hi >= lo:
+        mid = lo + (hi - lo) // 2
+
+        if lst[mid] == val:
+            return mid
+
+        if lst[mid] > val:
+            hi = mid - 1
+        elif lst[mid] < val:
+            lowest = mid
+            lo = mid + 1
+
+    return lowest
 
 
 # from stencil import *
 
+# included tests
 def test_recurse_1():
     # assert recurse([1, 2, 3]) == "1 2 3 "
     assert recurse([1, 2, 3]) == "1 2 3"
@@ -148,3 +163,101 @@ def test_find_closest_2():
 
 def test_find_closest_3():
     assert find_closest([1, 5, 10, 20, 100], 0) == -1
+
+
+# additional tests
+# not comprehensive
+
+# binary search
+def test_custom_find_index_1():
+    assert find_index([8], 8) == 0
+
+
+def test_custom_find_index_2():
+    assert find_index([8], 7) == -1
+
+
+def test_custom_find_index_3():
+    assert find_index([2, 4], 4) == 1
+
+
+def test_custom_find_index_4():
+    assert find_index([3, 9], 3) == 0
+
+
+def test_custom_find_index_5():
+    assert find_index([2, 7, 12], 7) == 1
+
+
+def test_custom_find_index_6():
+    assert find_index([2, 7, 12], 2) == 0
+
+
+def test_custom_find_index_7():
+    assert find_index([2, 7, 12], 12) == 2
+
+
+def test_custom_find_index_9():
+    assert find_index([-1, 0, 3, 5, 9, 12], 11) == -1
+
+
+def test_custom_find_index_10():
+    assert find_index([-1, 0, 3, 5, 9, 12], 12) == 5
+
+
+def test_custom_find_index_11():
+    assert find_index([-1, 0, 3, 5, 9, 12], 13) == -1
+
+
+# closest, from left, exclusive
+def test_custom_find_closest_1():
+    assert find_closest([1, 5, 10, 20, 100], 1) == 0
+
+
+def test_custom_find_closest_2():
+    assert find_closest([2, 5, 10, 20, 100], 1) == -1
+
+
+def test_custom_find_closest_3():
+    assert find_closest([2, 5, 10, 20, 100], 3) == 0
+
+
+def test_custom_find_closest_4():
+    assert find_closest([2, 5, 10, 20, 100], 4) == 0
+
+
+def test_custom_find_closest_5():
+    assert find_closest([2, 5, 10, 20, 100], 99) == 3
+
+
+def test_custom_find_closest_6():
+    assert find_closest([2, 5, 10, 20, 100], 100) == 4
+
+
+def test_custom_find_closest_7():
+    assert find_closest([2, 5, 10, 20, 100], 101) == 4
+
+
+# even ?
+def test_custom_find_closest_8():
+    assert find_closest([2, 5, 10, 20], 1) == -1
+
+
+def test_custom_find_closest_9():
+    assert find_closest([2, 5, 10, 20], 3) == 0
+
+
+def test_custom_find_closest_10():
+    assert find_closest([2, 5, 10, 20], 4) == 0
+
+
+def test_custom_find_closest_11():
+    assert find_closest([2, 5, 10, 20], 19) == 2
+
+
+def test_custom_find_closest_12():
+    assert find_closest([2, 5, 10, 20], 20) == 3
+
+
+def test_custom_find_closest_13():
+    assert find_closest([2, 5, 10, 20], 21) == 3
