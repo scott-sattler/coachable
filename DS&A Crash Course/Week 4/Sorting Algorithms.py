d73488afe58996cdae6f7599594c8e0fa43eb58a
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2023-08-10 14:36:59 -0500

    added mergesort (td/bu) templates

--- DS&A Crash Course/Week 4/Sorting Algorithms.py
+++ DS&A Crash Course/Week 4/Sorting Algorithms.py
@@ -32,34 +32,41 @@ def selection_sort(collection: list, debug=False) -> str:
     return ''.join(cr)
 
 
-def quicksort(collection: list, debug=False) -> str:
+def quicksort(collection: list, three_way=False, debug=False) -> str:
     ''' leftmost pivot '''  # noqa
     cr = collection
     if debug: print(''.join(cr))  # noqa
     n = len(cr)
-    _quicksort(cr, 0, n - 1, debug)
+    _quicksort(cr, 0, n - 1, three_way, debug)
     return ''.join(cr)
 
 
-def _quicksort(arr, lo, hi, debug):
+def _quicksort(arr, lo, hi, three_way, debug):
     if lo >= hi:
         return
 
-    pivot = partition(arr, lo, hi)
+    pivot = partition(arr, lo, hi, three_way)
+
+    # if debug:  print(''.join(arr))  # noqa
+
+    _quicksort(arr, lo, pivot - 1, debug, three_way)
+    _quicksort(arr, pivot + 1, hi, debug, three_way)
 
     if debug:  print(''.join(arr))  # noqa
 
-    _quicksort(arr, lo, pivot - 1, debug)
-    _quicksort(arr, pivot + 1, hi, debug)
 
-
-def partition(arr, lo, hi) -> int:
+def partition(arr, lo, hi, three_way) -> int:
     next_swap = lo + 1
 
     for i in range(lo + 1, hi + 1):
-        if arr[i] < arr[lo]:
-            arr[i], arr[next_swap] = arr[next_swap], arr[i]
-            next_swap += 1
+        if three_way:
+            if arr[i] <= arr[lo]:
+                arr[i], arr[next_swap] = arr[next_swap], arr[i]
+                next_swap += 1
+        else:
+            if arr[i] < arr[lo]:
+                arr[i], arr[next_swap] = arr[next_swap], arr[i]
+                next_swap += 1
 
     swap = next_swap - 1
     arr[lo], arr[swap] = arr[swap], arr[lo]
@@ -77,8 +84,10 @@ def mergesort_td():
 
 # assert insertion_sort(cr_list[:]) == 'AABCCCEHKLOORS'
 # assert selection_sort(cr_list[:]) == 'AABCCCEHKLOORS'
-assert quicksort(cr_list[:]) == 'AABCCCEHKLOORS'
+# assert quicksort(cr_list[:]) == 'AABCCCEHKLOORS'
+assert quicksort(cr_list[:], True) == 'AABCCCEHKLOORS'
 
 # insertion_sort(cr_list[:], True)
 # selection_sort(cr_list[:], True)
-quicksort(cr_list[:], True)
+# quicksort(cr_list[:], three_way=False, debug=True)
+quicksort(cr_list[:], three_way=True, debug=True)
