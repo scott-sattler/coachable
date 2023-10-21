29a5bb686c82a40b764e13a576b66ff84729a08e
Scott Sattler <scott.sattler.dev@gmail.com>
2023-10-20 21:58:17 -0500

    formatting

--- DS&A Crash Course/Week 7/Week 7 Practice.py
+++ DS&A Crash Course/Week 7/Week 7 Practice.py
@@ -18,7 +18,15 @@ Free Response Questions
         binary trees with subproblems solution to each subtree
 
     3. What is the difference between top-down and bottom-up dynamic programming?
-        todo
+        top down: assume the subproblems
+        bottom up: compose the subproblems
+
+        top down typically uses memoization with a naive recursive solution,
+        while bottom up typically uses an iterative approach, with tabulation.
+
+        bottom up solutions usually order the solutions by "size", solving the "smaller" subproblems first.
+        bottom up often has a superior constant factor, from lower overhead
+
 
     4. “Memoization” can be thought of as “caching” all the recursive calls that have already happened. What might be a
     reason why I wouldn’t want to do that?
@@ -30,7 +38,7 @@ Free Response Questions
 
     6. What if we had a f(r, c) that relied only on f(r-1, c) for some arbitrary c? What kind of space/memory
     optimization can we do if this is the case? If it helps, you can give an example of a specific problem.
-        todo
+        only cache f(r-1, c), with each new r value overwriting the previous data
 
     7. Fibonacci is one example of a 1-dimensional recurrence relation optimized with dynamic programming. Identify and
     share 3 other classes of dynamic programming problems that seem similar and describe what makes them feel similar.
@@ -48,9 +56,9 @@ Dynamic Programming and Recurrence Examples
     2. Recurrence Relation.  Identify the recurrence relationship and base cases. Explain why they are true by giving a
     qualitative explanation in plain English.
 
-    3. Bottom Up. Compute a small example using a bottom-up (n = 6, m = 6)
+    3. Bottom Up. Compute a small example using a bottom-up (n = 6, m = 6).
 
-    4. Top-Down Approach. Compute a small example using a top-down approach with memorization. (n=6,m=6)
+    4. Top-Down Approach. Compute a small example using a top-down approach with memoization (n = 6, m = 6).
 
     5. Complexity Analysis. Identify the optimal runtime and space complexity.
 
@@ -58,25 +66,87 @@ Dynamic Programming and Recurrence Examples
 
 Problems That Can Be Solved with Recursion
     1. Number of paths up a staircase of length N where you take 1 or 2 steps each time. Example provided.
+        1. N -> paths: 1 -> 1, 2 -> 2, 3 -> 3, 4 -> 5
+        2. f(n) = f(n - 1) + f(n - 2)
+        3. f(n) = table[i - 1] + table[i - 2]
+           [0, 0, 0, 0, 0, 0]
+           [1, 2, 0, 0, 0, 0]
+           [1, 2, 3, 0, 0, 0]
+           [1, 2, 3, 5, 0, 0]
+           [1, 2, 3, 5, 8, 0]
+           [1, 2, 3, 5, 8, 13]
+        4. call stack:
+           f(0) = 1
+           f(1) = 1
+           f(2) = 2
+           f(3) = 3
+           f(4) = 5
+           f(5) = 8
+           f(6) = 13
+        5. O(n) time; O(n) space
+        6. Yes; The recursive approach recomputes function calls.
 
     2. Computing the number of permutations of [1-n] i.e. [1,2,3,4,5,...n-1,n].
+        1. ordered combinations
+        2. f(n) = f( no next ) + f( next )
+        3.
+        4.
+        5.
+        6.
+
 
     3. Unique Paths: The number of paths from the top left corner of a grid to the bottom right corner when moving only
     down and to the right.
+        1. (n, m) -> paths: (0, 0) -> 0, (1, 1) -> 2, (2, 2) -> 2, (3, 3) -> 6
+        2.
+        3.
+        4.
+        5.
+        6.
 
     4. Given a 2xN grid, how many different ways can you fill the gird with 2x1 dominoes?
+        1.
+        2.
+        3.
+        4.
+        5.
+        6.
 
     5. Given a 3xN grid, how many different ways can you fill the gird with 3x1 dominoes?
+        1.
+        2.
+        3.
+        4.
+        5.
+        6.
 
     6. Given a set A = {1,2,3,...,N} calculate the number of possible subsets of A.
+        1.
+        2.
+        3.
+        4.
+        5.
+        6.
 
     7. Given a set A = {1,2,3,...,N} calculate the number of possible subsets of A that do not contain any 2 numbers
     that are 1 apart. For example, {1,2,4}  would not be valid because 1 and 2 are 1 apart.
+        1.
+        2.
+        3.
+        4.
+        5.
+        6.
 
     8. Count the number of functions from {1,2,3,...,N} to a set of size {1,2,3,...,M}. Here is an additional
     explanation of functions.
+        1.
+        2.
+        3.
+        4.
+        5.
+        6.
 
-    9. A function has a fixed point if f(x) = x for any x in the domain of f . How many functions are there from
+    9. A function has a fixed point if f(x) = x for any x in the domain of f. How many functions are there from
     {1,2,3,...,N} to {1,2,3,...,M} without any fixed points? Hint, approach the problem in cases, then put it all
     together.
         a. Case 1: Assume that M=N
@@ -105,13 +175,13 @@ Fibonacci Example (Problem 1):
     from n-2 . Therefore, the number of paths to n is the total number of paths from n-1 plus the number of paths from
     n-2.
 
-    f(0) = 1, f(1) = 1, f(2) = 2
-    f(3) = f(2) + f(1) = 3
-    f(4) = f(3) + f(2) = 5
-    f(5) = f(4) + f(3) = 8
-    f(6) = f(5) + f(4) = 13
+    3. f(0) = 1, f(1) = 1, f(2) = 2
+       f(3) = f(2) + f(1) = 3
+       f(4) = f(3) + f(2) = 5
+       f(5) = f(4) + f(3) = 8
+       f(6) = f(5) + f(4) = 13
 
-    1. This shows the call stack.
+    4. This shows the call stack.
     The bottom call `f(2)` is completed first.
 
     f(6) = f(5) + f(4) = 13 #f(6)=13 is stored in the memoization.
@@ -120,22 +190,46 @@ Fibonacci Example (Problem 1):
     f(3) = f(2) + f(1) = 3  #f(3)=3 is stored in the memoization.
     f(2) = f(1) + f(0) = 2  #f(2)=2 is stored in the memoization.
 
-    2. Runtime is O(n) and space O(1) if we only cache the previous 2 elements.
+    5. Runtime is O(n) and space O(1) if we only cache the previous 2 elements.
 
-    3. Yes. If we used plain recursion, the runtime would be exponential. Dynamic programming gets us an O(n) runtime.
+    6. Yes. If we used plain recursion, the runtime would be exponential. Dynamic programming gets us an O(n) runtime.
 
 OPTIONAL: Recursive Challenge Problems
     The following are very tough and completely optional.
     Hint: For these, you can assume the distinct integers are 1,2,3,...,N Moreover, 8-11 are very tricky, so don't
     hesitate to utilize external references. However, make sure you understand how they arrived at those solutions.
     1. Given a 4xN grid, how many different ways can you fill the grid with 2x1 dominoes?
+        1.
+        2.
+        3.
+        4.
+        5.
+        6.
 
     2. How many binary trees can you create with N distinct integers? Assume N=2^n-1 i.e. so N is the number of elements
     in a complete binary tree.
+        1.
+        2.
+        3.
+        4.
+        5.
+        6.
 
     3. How many binary search trees can you create with N=2^n-1 distinct integers?
+        1.
+        2.
+        3.
+        4.
+        5.
+        6.
 
     4. How many distinct binary heaps can you  create with N=2^n-1 distinct integers?
+        1.
+        2.
+        3.
+        4.
+        5.
+        6.
 
     5. We say a binary tree is level-sorted if every element is larger than every element in a lower level (even if they
     are in different subtrees).  Note all level-sorted trees are also heaps, but not all heaps are level-sorted. Notice
@@ -150,7 +244,14 @@ OPTIONAL: Recursive Challenge Problems
       1  2 6  7
 
     How many binary trees can you create with N=2^n-1 distinct integers that are level sorted?
+        1.
+        2.
+        3.
+        4.
+        5.
+        6.
 
 Challenge: Now solve 8-11 without assuming N=2^n-1
+    todo
 
 """
