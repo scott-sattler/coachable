64e1f1a95bfddd2356a8c9a5bce1c63c86b20a01
Scott Sattler <scott.sattler.dev@gmail.com>
2023-11-21 17:58:05 -0600

    initial commit

---
+++ DS&A Crash Course/Week 8 Final/Anagram and Close Strings.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+from typing import *
+
+'''
+Determine if these 2 strings are anagrams in O(n) runtime
+
+You can assume that a,b are strings with only letters.
+You should ignore capitalizations.
+
+For example,
+check_anagram("inTegRal", "Triangle") -> True
+'''
+
+
+def check_anagram(a: str, b: str) -> bool:
+    pass
+
+
+'''
+Given 2 strings determine how many full and partial matches they have.
+
+a = BEARS
+b = BEAST
+-> (3,1)
+
+3 full matches because BEA
+1 partial match since S is in the remaining part of the string but incorrect index.
+
+a = BANANA
+b = ORANGE
+-> (0, 2)
+
+0 full matches and 2 partial matches for AN.
+
+Return as a tuple (full matches, partial matches)
+'''
+
+
+def close_strings (a: str, b: str) -> tuple:
+    pass
