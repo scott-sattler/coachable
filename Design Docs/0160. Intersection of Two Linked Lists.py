06275adf2e6e2975c63b6c435e3e778bf5c12cfc
Scott Sattler <scott.sattler.dev@gmail.com>
2024-01-03 17:08:14 -0600

    live loom solution

---
+++ Design Docs/0160. Intersection of Two Linked Lists.py
@@ -0,0 +1,50 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    # noinspection PyMethodMayBeStatic,PyPep8Naming
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        # initialize pointers
+        # and counters
+        p1, p2 = headA, headB
+        c1, c2 = 0, 0
+
+        # find terminal nodes
+        while p1.next:
+            p1 = p1.next
+            c1 += 1
+        while p2.next:
+            p2 = p2.next
+            c2 += 1
+
+        # return null in the
+        # absence of an intersection
+        if p1 != p2:
+            return None
+
+        # reset pointers and align
+        # at equal distances to
+        # intersecting node
+        p1, p2 = headA, headB
+        while c1 > c2:
+            p1 = p1.next
+            c1 -= 1
+        while c2 > c1:
+            p2 = p2.next
+            c2 -= 1
+
+        # traverse linked lists
+        # until intersecting node
+        # is found
+        while p1 != p2:
+            p1 = p1.next
+            p2 = p2.next
+
+        return p1
