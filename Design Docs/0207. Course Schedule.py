12404f3cdba5920a60c408b9d60523fb97bf2631
Scott Sattler <scott.sattler.dev@gmail.com>
2024-01-07 18:01:50 -0600

    stylistic changes

--- Design Docs/0207. Course Schedule.py
+++ Design Docs/0207. Course Schedule.py
@@ -12,20 +12,19 @@ class Solution:
         out_deg = {i: [] for i in range(numCourses)}
 
         # find in/out degrees
-        for pre_req in prerequisites:
-            child = pre_req[0]
-            parent = pre_req[1]
+        for child, parent in prerequisites:
             in_deg[child] += 1
             out_deg[parent].append(child)
 
         # seed agenda with zero in-degrees
         agenda = list()  # could also use queue
-        for i in range(len(in_deg)):
+        for i in range(numCourses):
             if in_deg[i] == 0:
                 agenda.append(i)
 
-        # consume agenda by decrementing children's incoming
-        # degrees, and adding zero in-degrees to agenda
+        # consume agenda
+        # decrement children's incoming degrees
+        # add zero in-degree children to agenda
         while agenda:
             next_ = agenda.pop()
             topo.append(next_)
@@ -34,8 +33,5 @@ class Solution:
                 if in_deg[child] == 0:
                     agenda.append(child)
 
-        # return True iff every element was 
-        # able to be topologically sorted
-        if len(topo) == numCourses:
-            return True
-        return False
+        # return True iff every element can be topologically ordered
+        return True if len(topo) == numCourses else False
