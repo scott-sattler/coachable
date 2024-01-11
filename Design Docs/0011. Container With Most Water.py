c3f22f055698274748a17b304ceb2c87a2df7bc8
Scott Sattler <scott.sattler.dev@gmail.com>
2024-01-10 18:36:49 -0600

    live loom solution; modified for IDE compliance

---
+++ Design Docs/0011. Container With Most Water.py
@@ -0,0 +1,39 @@
+from typing import List
+
+
+class Solution:
+    # noinspection PyPep8Naming,PyMethodMayBeStatic
+    def maxArea(self, height: List[int]) -> int:
+        # two pointer solution:
+        # starting with the pointers in positions that
+        # can produce the largest possible area (0, n-1),
+        # we have found the maximum area of the shorter
+        # height. we therefore repeatedly increment,
+        # inward, the shorter of the two pointers.
+
+        # initialize pointers and max store
+        p1 = 0
+        p2 = len(height) - 1
+        max_area = float('-inf')
+
+        # iterate until p1 and p2 collide
+        while p1 < p2:
+            width = p2 - p1
+            # bound by the lesser height of p1
+            if height[p1] < height[p2]:
+                next_area = height[p1] * width
+                p1 += 1
+            # bound by p2
+            elif height[p2] < height[p1]:
+                next_area = height[p2] * width
+                p2 -= 1
+            # maximum of both p1 and p2 found
+            else:  # height[p1] == height[p2]
+                next_area = height[p1] * width
+                p1 += 1
+                p2 -= 1
+            # update if new area is larger
+            if next_area > max_area:
+                max_area = next_area
+
+        return max_area
