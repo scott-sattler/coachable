a44b6fa8201b78dd2dfab8042f9fb2932fa97ec6
Scott Sattler <scott.sattler.dev@gmail.com>
2024-02-15 01:44:04 -0600

    optimal solution added: O(n) -> O(1)

--- LC Mock/0237. Delete Node in a Linked List.py
+++ LC Mock/0237. Delete Node in a Linked List.py
@@ -5,6 +5,18 @@
 #         self.next = None
 
 class Solution:
+    # O(1) time
+    # swap (overwrite) and delete (dereference)
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+
+    # O(n) time
+    # left shift
     def deleteNode(self, node):
         """
         :type node: ListNode
