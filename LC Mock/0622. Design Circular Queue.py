4f33f7c96c991893ee1e17121dbd1350c77ee3ec
Scott Sattler <scott.sattler.dev@gmail.com>
2024-02-15 23:16:31 -0600

    live loom solution; added array variant

--- LC Mock/0622. Design Circular Queue.py
+++ LC Mock/0622. Design Circular Queue.py
@@ -1,7 +1,53 @@
+############################ array solution ############################ # noqa
 
+class MyCircularQueue:
+    def __init__(self, k: int):
+        self.size = 0
+        self.head = 0
+        self.tail = 0
+        self.cque = [-1] * k
+
+    def enQueue(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.cque[self.tail] = value
+        self.tail = (self.tail + 1) % len(self.cque)
+        self.size += 1
+        return True
+
+    def deQueue(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.cque[self.head] = -1
+        self.head = (self.head + 1) % len(self.cque)
+        self.size -= 1
+        return True
+
+    def Front(self) -> int:
+        return self.cque[self.head]
+
+    def Rear(self) -> int:
+        return self.cque[(self.tail - 1) % len(self.cque)]
+
+    def isEmpty(self) -> bool:
+        return self.size == 0
+
+    def isFull(self) -> bool:
+        return self.size == len(self.cque)
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
 
 
 ############################# node solution ############################ # noqa
+
 class Node:
     def __init__(self, val, nxt=None):
         self.val = val
