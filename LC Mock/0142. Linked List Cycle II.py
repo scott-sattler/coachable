c2bca9fe2af113d97fe347138c22d6e967c66f87
Scott Sattler <scott.sattler.dev@gmail.com>
2024-02-19 02:49:40 -0600

    improved variant

--- LC Mock/0142. Linked List Cycle II.py
+++ LC Mock/0142. Linked List Cycle II.py
@@ -1,3 +1,38 @@
+########################## more clear variant ########################## # noqa
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        # Floyd's Cycle Detection
+        slow = head
+        fast = head
+        while True:
+            if not fast or not fast.next:
+                return None
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+
+        # find cycle entry node
+        probe = head
+        while probe != slow:
+            probe = probe.next
+            slow = slow.next
+
+        return probe
+
+
 ############################ first solution ############################ # noqa
 
 # Definition for singly-linked list.
