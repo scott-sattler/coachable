974b65b32ad8a77097b6dde1569980aa70f10210
Scott Sattler <scott.sattler.dev@gmail.com>
2024-02-23 21:28:24 -0600

    live loom solution (recursive)

--- LC Mock/0206. Reverse Linked List.py
+++ LC Mock/0206. Reverse Linked List.py
@@ -1,4 +1,32 @@
 ###############################################################################
+# recursive solution
+# time: O(n)
+# space: O(1)
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        return self._hf(head, None)
+
+    def _hf(self, node, previous):
+        if not node:
+            return previous
+
+        next_node = node.next
+        node.next = previous
+
+        return self._hf(next_node, node)
+
+
+###############################################################################
 # iterative solution
 # time: O(n)
 # space: O(1)
