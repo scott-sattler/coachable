1c9e9f82725ee869f4e42c2da512b8c08ba5cca1
Scott Sattler <scott.sattler.dev@gmail.com>
2024-03-01 18:24:56 -0600

    toy recursive solution

--- LC Mock/0020. Valid Parentheses.py
+++ LC Mock/0020. Valid Parentheses.py
@@ -1,4 +1,40 @@
 ###############################################################################
+# toy recursive solution
+# time: O(n)
+# space:
+#   O(n) stack
+#   O(n) call stack
+#   O(n) auxiliary
+
+class Solution:
+    lookup = {
+        ')': '(',
+        '}': '{',
+        ']': '[',
+    }
+
+    def isValid(self, s: str) -> bool:
+        outer_stack = list()
+        return self._isValid(0, outer_stack, s)
+
+    def _isValid(self, i, stack, s):
+        if i >= len(s):
+            if stack:
+                return False
+            return True
+
+        if s[i] in '({[':
+            stack.append(s[i])
+        elif not stack:
+            return False
+        # pop stack and copare to current char
+        elif stack.pop() != self.lookup[s[i]]:
+            return False
+
+        return self._isValid(i + 1, stack, s)
+
+
+###############################################################################
 # revised iterative solution
 # time: O(n)
 # space:
