4130c34ed25abc60d8979acdf0a1c5c3cded7d79
Scott Sattler <scott.sattler.dev@gmail.com>
2024-03-01 18:24:13 -0600

    revised solution

--- LC Mock/0020. Valid Parentheses.py
+++ LC Mock/0020. Valid Parentheses.py
@@ -1,3 +1,41 @@
+###############################################################################
+# revised iterative solution
+# time: O(n)
+# space:
+#   O(n) stack
+#   O(n) auxiliary
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = list()
+        lookup = {
+            ')': '(',
+            '}': '{',
+            ']': '[',
+        }
+
+        for char in s:
+            if char in '({[':
+                stack.append(char)
+            elif len(stack) < 1:
+                return False
+            elif lookup[char] != stack.pop():
+                return False
+
+        if len(stack) > 0:
+            return False
+
+        return True
+
+
+###############################################################################
+# live iterative solution
+# time: O(n)
+# space:
+#   O(n) stack
+#   O(n) auxiliary
+
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = list()
