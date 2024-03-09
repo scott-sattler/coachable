3b2a7ed3564c233d0f9dee6af99612409e1a2de6
Scott Sattler <scott.sattler.dev@gmail.com>
2024-03-08 18:00:46 -0600

    live solution

--- LC Mock/0092. Reverse Linked List II.py
+++ LC Mock/0092. Reverse Linked List II.py
@@ -1,4 +1,78 @@
 ###############################################################################
+# live practice
+# time: O(n)
+# space: O(1)
+
+'''
+left = 2
+right = 4
+             .         .
+preh -> 1 -> 2 -> 3 -> 4 -> 5
+        ^    ^
+
+preh -> 1 |- 2 <- 3 <- 4    5
+        ^              ^    ^
+
+preh -> 1 -> 4 -> 3 -> 2 -> 5
+
+
+1 -> 2 -> 3 -> 4 -> 5 -|
+^                   ^  ^
+.    .
+1 -> 2
+
+# edge cases:
+    if not head.next or right == left:
+        return head
+
+    - reversing the entire list
+
+# traverse to (left - 1)
+# reverse from left to right (right - left +? 1)
+# link the surrounding nodes
+# return prehead.next
+
+time: O(n)
+space: O(1)
+
+'''
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if not head.next or right == left:
+            return head
+
+        prehead = ListNode(None, head)
+        pre_rev = prehead
+
+        # traverse to (left - 1)
+        for _ in range(left - 1):
+            pre_rev = pre_rev.next
+
+        # reverse from left to right
+        # (right - (left - 1))
+        curr = pre_rev.next
+        prev = None
+        for _ in range(right - left + 1):
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+
+        # link the surrounding nodes
+        pre_rev.next.next = next_node
+        pre_rev.next = prev
+
+        return prehead.next
+
+
+###############################################################################
 # live solution
 # time: O(c*n) -> O(n), where c is (right - left)
 # space: O(1)
